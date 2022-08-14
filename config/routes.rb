2327822d0e99581bde63f371ca89cf0b3473f762
MaksimGurov <guromaksim1334@gmail.com>
2022-08-14 11:49:33 +0300

    Rspec added + controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'static_pages/home'
+  get '/', to: 'static_pages#home'
   get 'static_pages/about'
   # Defines the root path route ("/")
   # root "articles#index"
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
-  describe "GET /home" do
+  describe "GET /" do
     it "returns http success" do
-      get "/static_pages/home"
+      get "/"
       expect(response).to have_http_status(:success)
     end
   end
