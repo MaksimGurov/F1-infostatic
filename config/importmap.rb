abcb3b8e93b28f573df7af7f48f8c3904a129204
MaksimGurov <guromaksim1334@gmail.com>
2022-08-15 22:43:58 +0300

    Add config importmap.rb bootstrap and routes.rb get news

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,6 +3,7 @@
 pin "application", preload: true
 
 pin "popper", to: 'popper.js', preload: true
+pin "bootstrap", to: 'bootstrap.min.js', preload: true
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
